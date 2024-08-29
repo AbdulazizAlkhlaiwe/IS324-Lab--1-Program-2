9f3bdd2f9eb5cdddec9d0bbebf3582040dc51e60
abdulaziz alkhlaiwe <140005102+AbdulazizAlkhlaiwe@users.noreply.github.com>
2024-08-29 21:27:20 +0300

    Add files via upload

---
+++ Lap1_program2/main.py
@@ -0,0 +1,5 @@
+total = float(input('Enter the amount of the purchase: \n'))
+print('Total purchase = ', total, ' SR')
+tax = total*0.15
+print('Sales tax = ',tax, 'SR')
+print('Total = ', total+tax, 'SR')
